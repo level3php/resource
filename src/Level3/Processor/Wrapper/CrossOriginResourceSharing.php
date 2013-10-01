9f4f1a45fea62f66c4e898e38931cd6e6f498744
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-10-01 19:40:16 +0200

    CORS wip

--- src/Level3/Processor/Wrapper/CrossOriginResourceSharing.php
+++ src/Level3/Processor/Wrapper/CrossOriginResourceSharing.php
@@ -6,7 +6,7 @@ use Level3\Messages\Request;
 use Level3\Messages\Response;
 
 use Level3\Processor\Wrapper;
-use Level3\Exceptions\HTTPException;
+use Level3\Exceptions\Forbidden;
 
 use RuntimeException;
 use Closure;
@@ -16,6 +16,12 @@ class CrossOriginResourceSharing extends Wrapper
 {
     const ALLOW_ORIGIN_WILDCARD = '*';
     const ALLOW_ORIGIN_HEADER = 'Access-Control-Allow-Origin';
+    const EXPOSE_HEADERS_HEADER = 'Access-Control-Expose-Headers';
+    const MAX_AGE_HEADER = 'Access-Control-Max-Age';
+    const ALLOW_CRENDENTIALS_HEADER = 'Access-Control-Allow-Credentials';
+    const ALLOW_METHODS_HEADER = 'Access-Control-Allow-Methods';
+    const ALLOW_HEADERS_HEADER = 'Access-Control-Allow-Headers';
+    const ORIGIN_HEADER = 'Origin';
 
     protected $allowOrigin;
     protected $exposeHeaders;
@@ -29,7 +35,7 @@ class CrossOriginResourceSharing extends Wrapper
         if (
             $allowOrigin && 
             $allowOrigin != self::ALLOW_ORIGIN_WILDCARD && 
-            !$this->isValidHost($host)
+            !$this->isValidHost($allowOrigin)
         ) {
             throw new RuntimeException('Malformed allow-origin, must be a host, null or *'); 
         }
@@ -50,7 +56,8 @@ class CrossOriginResourceSharing extends Wrapper
 
     protected function isValidHost($host)
     {
-        return (boolean) parse_url($host);
+        $url = parse_url($host);
+        return isset($url['scheme']) && isset($url['host']);
     }
 
     public function getAllowOrigin()
@@ -70,7 +77,7 @@ class CrossOriginResourceSharing extends Wrapper
 
     public function setMaxAge($maxAge)
     {
-        if ($maxAge && (int) $maxAge > 0) {
+        if ($maxAge && (int) $maxAge == 0) {
             throw new RuntimeException('Malformed max-age, must be a number or null'); 
         }
 
@@ -122,15 +129,46 @@ class CrossOriginResourceSharing extends Wrapper
 
     protected function processRequest(Closure $execution, Request $request, $method)
     {
+        $this->readRequestHeaders($request);
         $response = $execution($request);
-        $this->applyHeaders($response);
+        $this->applyResponseHeaders($response);
 
         return $response;
     }
 
-    protected function applyHeaders(Response $response)
+    protected function readRequestHeaders(Request $request)
+    {
+        $this->readOrigin($request);
+    }
+
+    protected function readOrigin(Request $request)
+    {
+        if ($this->allowOrigin === null) {
+            return;
+        }
+
+        $header = $request->getHeader(self::ORIGIN_HEADER);
+        if ($header) {
+            if ($this->allowOrigin == self::ALLOW_ORIGIN_WILDCARD) {
+                return;
+            }
+
+            if (in_array($header, (array) $this->allowOrigin)) {
+                return;
+            }    
+        }
+
+        throw new Forbidden();
+    }
+
+    protected function applyResponseHeaders(Response $response)
     {
         $this->applyAllowOriginHeader($response);
+        $this->applyExposeHeaders($response);
+        $this->applyMaxAge($response);
+        $this->applyAllowCredentials($response);
+        $this->applyAllowMethods($response);
+        $this->applyAllowHeaders($response);
     }
 
     protected function applyAllowOriginHeader(Response $response)
@@ -139,14 +177,61 @@ class CrossOriginResourceSharing extends Wrapper
             return;
         }
 
-        $origins = $this->allowOrigin;
-        if (!is_array($origins)) {
-            $origins = array($origins);
-        }
-
-        foreach ($origins as $origin) {
+        foreach ((array) $this->allowOrigin as $origin) {
             $response->addHeader(self::ALLOW_ORIGIN_HEADER, $origin);
         }
     }
-    
+
+    protected function applyExposeHeaders(Response $response)
+    {
+        if ($this->exposeHeaders === null) {
+            return;
+        }
+
+        $header = implode(', ',$this->exposeHeaders);
+        $response->addHeader(self::EXPOSE_HEADERS_HEADER, $header);
+    }
+
+    protected function applyMaxAge(Response $response)
+    {
+        if ($this->maxAge === null) {
+            return;
+        }
+
+        $response->addHeader(self::MAX_AGE_HEADER, $this->maxAge);
+    }
+
+    protected function applyAllowCredentials(Response $response)
+    {
+        if ($this->allowCredentials === null) {
+            return;
+        }
+
+        $header = 'false';
+        if ($this->allowCredentials) {
+            $header = 'true';
+        }
+
+        $response->addHeader(self::ALLOW_CRENDENTIALS_HEADER, $header);
+    }
+
+    protected function applyAllowMethods(Response $response)
+    {
+        if ($this->allowMethods === null) {
+            return;
+        }
+
+        $header = implode(', ',$this->allowMethods);
+        $response->addHeader(self::ALLOW_METHODS_HEADER, $header);
+    }
+
+    protected function applyAllowHeaders(Response $response)
+    {
+        if ($this->allowHeaders === null) {
+            return;
+        }
+
+        $header = implode(', ',$this->allowHeaders);
+        $response->addHeader(self::ALLOW_HEADERS_HEADER, $header);
+    }
 }
--- tests/Level3/Tests/Processor/Wrapper/CrossOriginResourceSharingTest.php
+++ tests/Level3/Tests/Processor/Wrapper/CrossOriginResourceSharingTest.php
@@ -18,21 +18,201 @@ class CrossOriginResourceSharingTest extends TestCase
         return new CORS();
     }
 
+    protected function createRequestMockWithGetHeader($header, $value)
+    {
+        $request = $this->createRequestMockSimple();
+        $request->shouldReceive('getHeader')
+            ->with($header)->once()->andReturn($value);
+
+        return $request;
+    }
+
+    protected function callGetInWrapperAndGetResponse($wrapper, $request = null)
+    {
+        if (!$request) $request = $this->createRequestMockSimple();
+        
+        return $wrapper->get(function($request) {
+            return new Response();
+        }, $request); 
+    }
+
     public function testSetAllowOriginWildcard()
     {
+        $url = 'http://foo.bar';
         $wrapper = $this->createWrapper();
-        $wrapper->setAllowOrigin('*');
+        $wrapper->setAllowOrigin(CORS::ALLOW_ORIGIN_WILDCARD);
 
-        $this->assertSame('*', $wrapper->getAllowOrigin());
+        $this->assertSame(CORS::ALLOW_ORIGIN_WILDCARD, $wrapper->getAllowOrigin());
 
-        $request = $this->createRequestMock();
-        $response = new Response();
+        $request = $this->createRequestMockWithGetHeader(CORS::ORIGIN_HEADER, $url);
+        $response = $this->callGetInWrapperAndGetResponse($wrapper, $request);
+        $this->assertSame(CORS::ALLOW_ORIGIN_WILDCARD, $response->getHeader(CORS::ALLOW_ORIGIN_HEADER));
+    }
 
-        $wrapper->get(function($request) use ($response) {
-            $request->getKey();
-            return $response;
-        }, $request);
+    public function testSetAllowOriginHost()
+    {
+        $url = 'http://foo.bar';
+        $wrapper = $this->createWrapper();
+        $wrapper->setAllowOrigin($url);
 
-        $this->assertSame('*', $response->getHeader(CORS::ALLOW_ORIGIN_HEADER));
+        $this->assertSame($url, $wrapper->getAllowOrigin());
+
+        $request = $this->createRequestMockWithGetHeader(CORS::ORIGIN_HEADER, $url);
+        $response = $this->callGetInWrapperAndGetResponse($wrapper, $request);
+        $this->assertSame($url, $response->getHeader(CORS::ALLOW_ORIGIN_HEADER));
+    }
+
+    /**
+     * @expectedException Level3\Exceptions\Forbidden
+     */
+    public function testReadOriginInvalid()
+    {
+        $url = 'http://foo.bar';
+        $requestUrl = 'http://baz.qux';
+
+        $wrapper = $this->createWrapper();
+        $wrapper->setAllowOrigin($url);
+
+        $request = $this->createRequestMockWithGetHeader(CORS::ORIGIN_HEADER, $requestUrl);
+        $response = $this->callGetInWrapperAndGetResponse($wrapper, $request);
+    }
+
+    /**
+     * @expectedException Level3\Exceptions\Forbidden
+     */
+    public function testReadOriginNone()
+    {
+        $allowOrigin = '*';
+
+        $wrapper = $this->createWrapper();
+        $wrapper->setAllowOrigin($allowOrigin);
+
+        $request = $this->createRequestMockWithGetHeader(CORS::ORIGIN_HEADER, null);
+        $response = $this->callGetInWrapperAndGetResponse($wrapper, $request);
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testSetAllowOriginNotValidHost()
+    {
+        $url = 'foobar';
+        $wrapper = $this->createWrapper();
+        $wrapper->setAllowOrigin($url);
+    }
+
+    public function testSetMultipleAllowOrigin()
+    {
+        $urls = array(
+            'http://foo.bar',
+            'http://qux.baz'
+        );
+
+        $wrapper = $this->createWrapper();
+        $wrapper->setMultipleAllowOrigin($urls);
+
+        $this->assertSame($urls, $wrapper->getAllowOrigin());
+
+        $request = $this->createRequestMockWithGetHeader(CORS::ORIGIN_HEADER, $urls[0]);
+        $response = $this->callGetInWrapperAndGetResponse($wrapper, $request);
+        $this->assertSame($urls, $response->getHeaders(CORS::ALLOW_ORIGIN_HEADER));
+    }
+
+    public function testSetExposeHeaders()
+    {
+        $headers = array('bar', 'foo');
+
+        $wrapper = $this->createWrapper();
+        $wrapper->setExposeHeaders($headers);
+
+        $this->assertSame($headers, $wrapper->getExposeHeaders());
+
+        $response = $this->callGetInWrapperAndGetResponse($wrapper);
+        $this->assertSame('bar, foo', $response->getHeader(CORS::EXPOSE_HEADERS_HEADER));
+    }
+
+    public function testSetMaxAge()
+    {
+        $maxAge = 100;
+
+        $wrapper = $this->createWrapper();
+        $wrapper->setMaxAge($maxAge);
+
+        $this->assertSame($maxAge, $wrapper->getMaxAge());
+
+        $response = $this->callGetInWrapperAndGetResponse($wrapper);
+        $this->assertSame($maxAge, $response->getHeader(CORS::MAX_AGE_HEADER));
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testSetMaxAgeNotANumber()
+    {
+        $invalidAge = 'foobar';
+        $wrapper = $this->createWrapper();
+        $wrapper->setMaxAge($invalidAge);
+    }
+
+    public function testSetAllowCredentialsTrue()
+    {
+        $allow = true;
+
+        $wrapper = $this->createWrapper();
+        $wrapper->setAllowCredentials($allow);
+
+        $this->assertSame($allow, $wrapper->getAllowCredentials());
+
+        $response = $this->callGetInWrapperAndGetResponse($wrapper);
+        $this->assertSame('true', $response->getHeader(CORS::ALLOW_CRENDENTIALS_HEADER));
+    }
+
+    public function testSetAllowCredentialsFalse()
+    {
+        $allow = false;
+
+        $wrapper = $this->createWrapper();
+        $wrapper->setAllowCredentials($allow);
+
+        $this->assertSame($allow, $wrapper->getAllowCredentials());
+
+        $response = $this->callGetInWrapperAndGetResponse($wrapper);
+        $this->assertSame('false', $response->getHeader(CORS::ALLOW_CRENDENTIALS_HEADER));
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testSetAllowCredentialsNonBoolean()
+    {
+        $invalidAllow = 'foobar';
+        $wrapper = $this->createWrapper();
+        $wrapper->setAllowCredentials($invalidAllow);
+    }
+
+    public function testSetAllowMethods()
+    {
+        $methods = array('bar', 'foo');
+
+        $wrapper = $this->createWrapper();
+        $wrapper->setAllowMethods($methods);
+
+        $this->assertSame($methods, $wrapper->getAllowMethods());
+
+        $response = $this->callGetInWrapperAndGetResponse($wrapper);
+        $this->assertSame('bar, foo', $response->getHeader(CORS::ALLOW_METHODS_HEADER));
+    }
+
+    public function testSetAllowHeaders()
+    {
+        $headers = array('bar', 'foo');
+
+        $wrapper = $this->createWrapper();
+        $wrapper->setAllowHeaders($headers);
+
+        $this->assertSame($headers, $wrapper->getAllowHeaders());
+
+        $response = $this->callGetInWrapperAndGetResponse($wrapper);
+        $this->assertSame('bar, foo', $response->getHeader(CORS::ALLOW_HEADERS_HEADER));
     }
 }
