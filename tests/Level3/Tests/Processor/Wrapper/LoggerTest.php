feb521c39672e0065c30b8e0dc36fad01d391fc8
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-10-12 13:46:19 +0200

    RequestLogger renamed to Logger

---
+++ tests/Level3/Tests/Processor/Wrapper/LoggerTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace Level3\Tests;
+
+use Level3\Processor\Wrapper\Logger;
+use Level3\Messages\Response;
+use Psr\Log\LogLevel;
+use Teapot\StatusCode;
+use Exception;
+
+use Mockery as m;
+
+class LoggerTest extends TestCase
+{
+    private $wrapper;
+
+    public function setUp()
+    {
+        $this->loggerMock = m::mock('Psr\Log\LoggerInterface');
+        $this->wrapper = new Logger($this->loggerMock);
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testExceptionHandling($method, $code, $level)
+    {
+        $attributes = $this->createParametersMock();
+        $request = $this->createRequestMock($attributes);
+        
+        $this->loggerMock->shouldReceive($level)
+            ->once()->andReturn(null);
+
+        $response = new Response();
+        $response->setStatusCode($code);
+
+        $expected = $this->wrapper->$method(function($request) use ($response, $code) {
+            return $response;
+        }, $request);
+
+        $this->assertSame($response, $expected);
+    }
+
+    public function provider()
+    {
+        return array(
+            array('get', StatusCode::OK, LogLevel::INFO),
+            array('get', StatusCode::NOT_FOUND, LogLevel::WARNING),
+            array('get', StatusCode::INTERNAL_SERVER_ERROR, LogLevel::ERROR),
+            array('find', StatusCode::OK, LogLevel::INFO),
+            array('find', StatusCode::NOT_FOUND, LogLevel::WARNING),
+            array('find', StatusCode::INTERNAL_SERVER_ERROR, LogLevel::ERROR),
+            array('post', StatusCode::OK, LogLevel::INFO),
+            array('post', StatusCode::NOT_FOUND, LogLevel::WARNING),
+            array('post', StatusCode::INTERNAL_SERVER_ERROR, LogLevel::ERROR),
+            array('patch', StatusCode::OK, LogLevel::INFO),
+            array('patch', StatusCode::NOT_FOUND, LogLevel::WARNING),
+            array('patch', StatusCode::INTERNAL_SERVER_ERROR, LogLevel::ERROR),
+            array('put', StatusCode::OK, LogLevel::INFO),
+            array('put', StatusCode::NOT_FOUND, LogLevel::WARNING),
+            array('put', StatusCode::INTERNAL_SERVER_ERROR, LogLevel::ERROR),
+            array('delete', StatusCode::OK, LogLevel::INFO),
+            array('delete', StatusCode::NOT_FOUND, LogLevel::WARNING),
+            array('delete', StatusCode::INTERNAL_SERVER_ERROR, LogLevel::ERROR),
+            array('options', StatusCode::OK, LogLevel::INFO)
+        );
+    }
+}
