2e520ab7d324d67e85b82533f1e80d3a71a21b7c
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-11-15 01:39:27 +0100

    hal implementation

--- src/Level3/Formatter/HALJsonFormatter.php
+++ src/Level3/Formatter/HALJsonFormatter.php
@@ -28,10 +28,80 @@ class HALJsonFormatter extends Formatter
     public function toResponse(Resource $resource, $pretty = false)
     {
         $options = JSON_UNESCAPED_SLASHES;
-        if ($pretty) {
+
+        if (!$pretty) {
             $options = $options | JSON_PRETTY_PRINT;
         }
 
-        return json_encode($resource->toArray(), $options);
+        return json_encode($this->resourceToArray($resource), $options);
+    }
+
+    protected function resourceToArray(Resource $resource)
+    {
+        $data = $resource->getData();
+
+        $this->transformResources($data, $resource);
+        $this->transformLinks($data, $resource);
+        $this->transformLinkedResources($data, $resource);
+
+        return $data;
+    }
+
+    protected function transformLinks(&$array, Resource $resource)
+    {
+        if ($self = $resource->getSelfLink()) {
+            $array['_links']['self'] = $self->toArray();
+        }
+
+        foreach ($resource->getAllLinks() as $rel => $links) {
+            if (count($links) == 1) {
+                $array['_links'][$rel] = end($links)->toArray();
+            } else {
+                foreach ($links as $link) {
+                    $array['_links'][$rel][] = $link->toArray();
+                }
+            }
+        }
+    }
+
+    protected function transformLinkedResources(&$array, Resource $resource)
+    {
+       foreach ($resource->getAllLinkedResources() as $rel => $links) {
+            if (count($links) == 1) {
+                $array['_links'][$rel] = end($links)->getSelfLink()->toArray();
+            } else {
+                foreach ($links as $link) {
+                    $array['_links'][$rel][] = $link->getSelfLink()->toArray();
+                }
+            }
+        }
+    }
+
+    protected function transformResources(&$array, Resource $resource)
+    {
+        $embedded = [];
+        foreach ($resource->getAllResources() as $rel => $resources) {
+            if ($resources instanceOf Resource) {
+                if (!$resources->getUri()) {
+                    $array[$rel] = $this->resourceToArray($resources);
+                } else {
+                    $embedded[$rel] = $this->resourceToArray($resources);
+                }
+
+                continue;
+            }
+
+            foreach ($resources as $resource) {
+                if (!$resource->getUri()) {
+                    $array[$rel][] = $this->resourceToArray($resource);
+                } else {
+                    $embedded[$rel][] = $this->resourceToArray($resource);
+                }
+            }
+        }
+
+        if ($embedded) {
+            $array['_embedded'] = $embedded;
+        }
     }
 }
